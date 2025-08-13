85d87230b07d4ef0f049b26a22b6d3fe264b791c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-13 10:16:36 +0000

    updates

--- end_exercises/5_setenv.c
+++ end_exercises/5_setenv.c
@@ -61,7 +61,6 @@ int _setenv(const char *name, const char *value, int overwrite)
 			/* env var exists - do not overwrite */
 			else if (overwrite == 0)
 			{
-
 				printf("overwrite is 0, env var not overwritten");
 				return (0);
 			}
@@ -69,4 +68,47 @@ int _setenv(const char *name, const char *value, int overwrite)
 		}
 		i++;
 	}
+/* env var doesn't exist - add */
+	newlen = length + strlen(value) + 2; /* find length of name+=+value+nullterm */
+	/* malloc space and crearte new name=value string */
+	char *new_var = malloc(newlen);
+	if (new_len == NULL)
+	{
+		printf("Error: malloc fail\n");
+		return (1);
+	}
+	snprintf(new_var, newlen, "%s=%s", name, value);
+
+	/* Count number of env vars again */
+	int count = 0;
+	while (environ[count] != NULL)
+		count++;
+
+	/* make new arrray for updated env list */
+	char **new_environ = malloc(sizeof(char *) * (count + 2));
+	if (new_environ == NULL)
+	{
+		free(new_var);
+		printf("Error: malloc failed\n");
+		return (1);
+	}
+
+	int j = 0;
+	/* copy existing env var to new array */
+	while (j < count)
+	{
+		new_environ[j] = environ[j];
+		j++;
+	}
+	/* add new env var string to end of array */
+	new_environ[count] = new_var;
+/* add NULL to end of array */
+	new_environ[count + 1] = NULL;
+
+	/* update environ to point to new env var array */
+	environ = new_environ;
+
+	printf("Env var added\n");
+	return (0);
+
 }
